35af1d8cc2cadfe9fd73c34e759ead474d724281
Higor Morais de Lima <moraislimahigor@outlook.com>
2021-10-20 04:26:21 -0300

    Tela AbountView

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -30,7 +39,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemAbountAction() {
-		System.out.println("Menu abount clicado.");
+		loadView("/gui/About.fxml");
 	}
 	
 	
@@ -39,5 +48,42 @@ public class MainViewController implements Initializable {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private synchronized void loadView(String absoluteName) {
+		
+		try {
+			//carregando a view a ser aberta nas 2 linhas abaixo...
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			//nas 2 linhas abaixo, pegamos o VBox principal
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			//nas 4 linhas abaixo: limpamos a vbox, recolocamos o menu da tela principal e os itens da vbox abount.
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
