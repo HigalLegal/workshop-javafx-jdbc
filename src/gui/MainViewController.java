6a54b466e2c48b4eb012d69c97807deb1d8f8ccb
Higor Morais de Lima <moraislimahigor@outlook.com>
2021-10-21 17:53:18 -0300

    Preenchendo a tabela Department com dados mockados.

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -15,75 +16,68 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartment;
-	
+
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("Menu do vendedor clicado.");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentrAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.upadateTableView();
+		});
 	}
-	
+
 	@FXML
 	public void onMenuItemAbountAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
-	
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	private synchronized void loadView(String absoluteName) {
-		
+
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
+
 		try {
-			//carregando a view a ser aberta nas 2 linhas abaixo...
+			// carregando a view a ser aberta nas 2 linhas abaixo...
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
-			
-			//nas 2 linhas abaixo, pegamos o VBox principal
+
+			// nas 2 linhas abaixo, pegamos o VBox principal
 			Scene mainScene = Main.getMainScene();
 			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-			
-			//nas 4 linhas abaixo: limpamos a vbox, recolocamos o menu da tela principal e os itens da vbox abount.
+
+			// nas 4 linhas abaixo: limpamos a vbox, recolocamos o menu da tela principal e
+			// os itens da vbox abount.
 			Node mainMenu = mainVBox.getChildren().get(0);
 			mainVBox.getChildren().clear();
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
+
+			//essas 2 linhas vão executar o segundo parâmetro
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 			
-		} catch(IOException e) {
+		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
